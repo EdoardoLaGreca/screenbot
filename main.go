e100f73f99d84c01f3847c0f53c59315ed78f945
EdoardoLaGreca <elagreca@protonmail.com>
2021-09-18 17:52:45 +0200

    Move offline directory creation at the beginning, add getErasedBoard function, add imgs directory, add the ability to send the images that couldn't be sent

--- main.go
+++ main.go
@@ -1,11 +1,12 @@
-package main
 
+package main
 import (
 	"os"
 	"fmt"
 	"log"
 	"time"
 	"image"
+	"image/png"
 	"runtime"
 	"github.com/EdoardoLaGreca/screenbot/analysis"
 	"github.com/EdoardoLaGreca/screenbot/network"
@@ -31,17 +32,44 @@ func main() {
 		"where (x1, y1) are the coordinates of the top-left corner and " +
 		"(x2, y2) are the coordinates of the bottom-right corner: ")
 
-	fmt.Scanf("%d %d %d %d", &area.Min.X, &area.Min.Y, &area.Max.X, &area.Max.Y)
+	// Check if the "offline" directory exists. If it doesn't, create it.
+	_, statErrOffline := os.Stat("offline")
+	if os.IsNotExist(statErrOffline) {
+		os.Mkdir("offline", 0755)
+	}
 
-	fmt.Println("\nAssuming that currently the board is empty...")
+	// Check if the "imgs" directory exists. If it doesn't, create it.
+	_, statErrImgs := os.Stat("imgs")
+	if os.IsNotExist(statErrImgs) {
+		os.Mkdir("imgs", 0755)
+	}
+
+	fmt.Scanf("%d %d %d %d", &area.Min.X, &area.Min.Y, &area.Max.X, &area.Max.Y)
 
 	var prevImg, erasedImg *image.RGBA
 
+	erasedImg, err := getErasedBoard()
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println("File erased.png found!")
+
+		if erasedImg.Bounds() != area {
+			fmt.Println("The image of file erased.png has different size " +
+				"(width and height) compared to the chosen coordinates, so it " +
+				"will be ignored.")
+			erasedImg = nil
+		}
+	}
+
 	// Get the first image
-	for {
-		erasedImg = getScreenshot(area)
-		if erasedImg != nil {
-			break
+	if erasedImg == nil {
+		fmt.Println("\nAssuming that currently the board is empty...")
+		for {
+			erasedImg = getScreenshot(area)
+			if erasedImg != nil {
+				break
+			}
 		}
 	}
 
@@ -71,21 +99,30 @@ func sendLoop(area image.Rectangle, prevImg, erasedImg *image.RGBA, url string) 
 
 		if !analysis.AreImgsEqual(prevImg, currImg) {
 			if analysis.BoardIsErased(currImg, erasedImg) {
+				// Name for the stored image
+				timestamp := time.Now().Format(time.RFC822)
+				imgName := timestamp + ".png"
 
+				// Store all images
+				storeImgErr(prevImg, "imgs/" + imgName)
+				
 				// Send image through a goroutine to avoid a blocking behaviour
-				go func() {
-					err := network.SendImg(url, prevImg)
+				go func(image *image.RGBA, filename string) {
+					err := network.SendImg(url, image)
 
 					if err != nil {
 						log.Println("Unable to send the image, error:", err)
 
 						// Save the image as local file and send it once there
 						// will be a connection again
-
+						storeImgErr(image, "offline/" + filename)
 					} else {
 						log.Println("Image sent!")
+
+						// Also send the images stored that has not been sent yet
+						network.SendStored()
 					}
-				}()
+				}(prevImg, imgName)
 
 			}
 
@@ -94,4 +131,32 @@ func sendLoop(area image.Rectangle, prevImg, erasedImg *image.RGBA, url string) 
 
 		time.Sleep(1*time.Second)
 	}
+}
+
+// Store image and print errors, if any
+func storeImgErr(img *image.RGBA, filename string) {
+	err := network.StoreImg(img, filename)
+	if err != nil {
+		log.Println("Unable to store image, error:", err)
+	}
+}
+
+// Get erased board image
+func getErasedBoard() (*image.RGBA, error) {
+	_, err := os.Stat("erased.png")
+	if err != nil {
+		msg := "The image for the erased board (erased.png) was not found, " +
+			"using the first screenshot as erased board."
+		return nil, fmt.Errorf(msg)
+	}
+
+	file, err := os.Open("erased.png")
+
+	// Decode the image into image.Image
+	img, err := png.Decode(file)
+	if err != nil {
+		return nil, err
+	}
+
+	return img.(*image.RGBA), nil
 }
